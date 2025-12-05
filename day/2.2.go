1b19e5a2ab1efcfd1f34656547d22fbac1684a35
colourmebrad <bradmallett@gmail.com>
2025-12-05 08:31:58 -0500

    2.2 working properly

--- day/2.2.go
+++ day/2.2.go
@@ -23,42 +23,36 @@ func (idRange IDRange) getRepeatedCycles() []int {
 }
 
 func hasRepeatedCycle(intString string) bool {
-	fmt.Printf("Checking %s\n", intString)
+	//fmt.Printf("Checking %s\n", intString)
 	sequence := ""
 	for y := 0; y < len(intString)-1; y++ {
 		sequence = sequence + string(intString[y]) // why do I have to string this, that's so annoying (oh, I bet it's unicode bullshit)
 
-		// the problem I'm running into is that this code works for 11 and 12341234 but erroneously returns true for 110
-		// I need to ensure there aren't any non-repeated characters
-		// 121212 is valid
-		// 12125 is not
-		// 11 is valid
-		// 211 is not and 112 is not
-
-		// maybe here is where we want another loop?
-		// we basically have to take sequence and then ensure that the rest of the strong is ONLY 1+ loops of that sequence
-
-		// is there enough string left for another of this sequence?
-		nextSequenceStart := y + 1
-		nextSequenceEnd := y + 1 + len(sequence)
-
-		if nextSequenceEnd < len(intString)+1 {
-			if sequence == intString[nextSequenceStart:nextSequenceEnd] {
-				fmt.Printf("%s == %s\n", sequence, intString[nextSequenceStart:nextSequenceEnd])
-				return true
-			} else {
-				//fmt.Printf("%s != %s\n", sequence, intString[nextSequenceStart:nextSequenceEnd])
-			}
-		} else {
-			//fmt.Printf("%d >= %d\n", nextSequenceEnd, len(intString))
+		if isRepeatedCycleOf(sequence, intString[y+1:]) {
+			return true
 		}
 	}
 
 	return false
 }
 
+func isRepeatedCycleOf(sequence string, input string) bool {
+	//fmt.Printf("isRepeatedCycleOf(%s, %s)\n", sequence, input)
+
+	if sequence == input {
+		return true
+	}
+
+	end := min(len(sequence), len(input))
+
+	if sequence == input[0:end] {
+		return isRepeatedCycleOf(sequence, input[len(sequence):])
+	}
+	return false
+}
+
 func TwoTwo() {
-	input := shared.GetPuzzleInput("2.example.txt", ",")
+	input := shared.GetPuzzleInput("2.txt", ",")
 
 	sum := 0
 
